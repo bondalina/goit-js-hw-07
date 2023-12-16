9467b0e50790e4d8b261901ebc939f07e092dc8a
bondalina <142501520+bondalina@users.noreply.github.com>
2023-12-16 16:15:30 +0200

    task 5 js

--- js/task-05.js
+++ js/task-05.js
@@ -3,3 +3,15 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const colorSpan = document.querySelector('.color');
+const changeColorButton = document.querySelector('.change-color');
+
+const body = document.body;
+
+changeColorButton.addEventListener('click', () => {
+  const randomColor = getRandomHexColor();
+
+  body.style.backgroundColor = randomColor;
+  colorSpan.textContent = randomColor;
+});
